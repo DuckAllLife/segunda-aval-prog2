0b899687733ed716cddfa9de93bbd161a8170cc5
Gabriel Benigno <gbenignorocha@gmail.com>
2022-07-29 10:59:09 -0300

    Aprendendo a ler e gravar arquivos no main e modificando a locaização da pasta de "examples"

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,14 @@
+import 'dart:io';
 
 void main() {
+
+  final inFile = File('example_files/example_csv.csv');
+  final outFile = File('example_csv_out.csv');
+
+  final content = inFile.readAsStringSync();
+  
+  outFile.createSync();
+  outFile.writeAsStringSync(content);
 
   print("Integrantes do Grupo: Celenny Cristhyne do Nascimento Souza, Gabriel Benigno Rocha, Richelle Kim Mota Bahia");
 

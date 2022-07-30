c67a5732463fa635b696261cfe0940defff9e570
Gabriel Benigno <gbenignorocha@gmail.com>
2022-07-30 15:18:49 -0300

    Algumas alterações

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,30 @@
-import 'csv_data.dart';
+import 'tsv_data.dart';
 
 void main() {
-  //final inFile = File('example_files/example_csv.csv');
-  //final outFile = File('example_csv_out.csv');
+  //final inFile = File('example_files/example_tsv.tsv');
+  //final outFile = File('example_tsv_out.tsv');
 
   //final content = inFile.readAsStringSync();
 
   //outFile.createSync();
   //outFile.writeAsStringSync(content);
 
-  CsvData test = CsvData();
-  test.load('example_files/example_csv.csv');
+  TsvData test = TsvData();
+  test.load('example_files/example_tsv.tsv');
   //print(test.separator);
   //print(test.hasData);
-  //print(test.fields);
+  print(test.fields);
 
-  //print(test.data);
+  print(test.data);
 
-  //test.data = 'ARFTGFFSAADSF';
-  //print(test.data);
-  print(test.content);
+  test.data = 'ARFTGFFSAADSF';
+  
+  //print(test.content);
   test.clear();
-  print(test.content);
+  //print(test.content);
+  print(test.data);
 
-  test.save('teste.csv');
+  test.save('teste.tsv');
 
   print(
       "\nIntegrantes do Grupo: Celenny Cristhyne do Nascimento Souza, Gabriel Benigno Rocha, Richelle Kim Mota Bahia");

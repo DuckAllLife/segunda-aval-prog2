2ac3fe327d2caea9f707bc52b0be8baeaeb94c87
Richelle <richellekim10@hotmail.com>
2022-07-30 17:26:49 -0300

    começo do xml

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'csv_data.dart';
+import 'tsv_data.dart';
+import 'xml_data.dart';
 
 void main() {
   //final inFile = File('example_files/example_csv.csv');
@@ -9,43 +11,22 @@ void main() {
   //outFile.createSync();
   //outFile.writeAsStringSync(content);
 
-<<<<<<< HEAD
-  CsvData test = CsvData();
-  test.load('example_files/csv-ex.csv');
-  print('Get data ' + test.data);
-  print('Test separator ' + test.separator);
-  print('has data ${test.hasData}');
-  print('Test fields ${test.fields}');
+  XmlData test = XmlData();
+  test.load('example_files/example_xml.xml');
+  //print('Get data ' + test.data);
+  //print('has data ${test.hasData}');
+  //print('Test fields ${test.fields}');
 
-  print('Get data ' + test.data); // get
+  //print('Get data ' + test.data); // get
 
-  test.data = 'ARFTGFFSAADSF'; // set
-  print('set data ${test.data}');
+  //test.data = 'ARFTGFFSAADSF'; // set
+  //print('set data ${test.data}');
 
-  test.clear();
-  print('has data ${test.data}');
-  print('Get data ' + test.data);
-=======
-  TsvData test = TsvData();
-  test.load('example_files/example_tsv.tsv');
+  //test.clear();
+  //print('has data ${test.data}');
+  //print('Get data ' + test.data);
 
-  print('separador: ${test.separator}');
-  print('hasdada: ${test.hasData}');
-  print('teste fields: ${test.fields}');
-
-  print('get: ${test.data}');
-
-  test.data = 'ARFTGFFSAADSF'; //set
-  print('get: ${test.data}');
-
-  test.clear();
-
-  print('get: ${test.data}');
-  print(test.lines);
-  print(test.hasData);
->>>>>>> 24e4b005783db07c7dce6c78bb4c8311ffafc80c
-
-  test.save('teste.tsv');
+  //test.save('teste.tsv');
 
   print(
       "\nIntegrantes do Grupo: Celenny Cristhyne do Nascimento Souza, Gabriel Benigno Rocha, Richelle Kim Mota Bahia");
--- lib/xml_data.dart
+++ lib/xml_data.dart
@@ -1,3 +1,29 @@
-class XmlData {
-  
+import 'dart:io';
+import 'package:xml/xml.dart';
+import 'Data.dart';
+
+class XmlData extends Data {
+  @override
+  void load(String fileName) {
+    content = File(fileName).readAsStringSync();
+    final xml = XmlDocument.parse(content);
+    final records = xml.findAllElements('record');
+    print(records);
+  }
+
+  @override
+  late String data;
+
+  @override
+  void clear() {
+    // TODO: implement clear
+  }
+
+  @override
+  // TODO: implement fields
+  List<String> get fields => throw UnimplementedError();
+
+  @override
+  // TODO: implement hasData
+  bool get hasData => throw UnimplementedError();
 }

a4ffab097ae4da9a959e56bd8eeeb6b51959bbc6
celenny <celennycs@gmail.com>
2022-07-30 15:16:39 -0300

    main and test,tsv

--- lib/json_data.dart
+++ lib/json_data.dart
@@ -1,3 +1,3 @@
-class JsonData {
+class JsonData  {
   
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'tsv_data.dart';
+import 'csv_data.dart';
 
 void main() {
   //final inFile = File('example_files/example_csv.csv');
@@ -9,19 +9,21 @@ void main() {
   //outFile.createSync();
   //outFile.writeAsStringSync(content);
 
-  TsvData test = TsvData();
-  test.load('example_files/example_tsv.tsv');
-  print(test.separator);
-  print(test.hasData);
-  print(test.fields);
+  CsvData test = CsvData();
+  test.load('example_files/csv-ex.csv');
+  print('Get data ' + test.data);
+  print('Test separator ' + test.separator);
+  print('has data ${test.hasData}');
+  print('Test fields ${test.fields}');
 
-  print(test.data);
+  print('Get data ' + test.data); // get
 
-  test.data = 'ARFTGFFSAADSF';
-  print(test.data);
+  test.data = 'ARFTGFFSAADSF'; // set
+  print('set data ${test.data}');
 
   test.clear();
-  print(test.hasData);
+  print('has data ${test.data}');
+  print('Get data ' + test.data);
 
   test.save('teste.tsv');
 
